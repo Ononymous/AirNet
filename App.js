28b154aac3d04b2e518d6f83595265ace0081798
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2023-02-17 18:02:16 -0800

    testing

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 //test
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Testing testing</Text>
+      <Button title="Click me" onPress={() => alert('Button clicked!')} />
+      <Button title='Test button' onPress={() => alert('Button clicked!')} />
+      <Text>Random text here test</Text>
     </View>
   );
 }
