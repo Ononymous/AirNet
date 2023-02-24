f2d510c0712cddaccc2b35a2ccfc3a971141fc91
Gen Tamada <gtamada@ucsb.edu>
2023-02-24 03:44:24 -0800

    Added functionality to get real time location of user
    Use Expo location to get geolocation of device
    Now boundary box is no longer hardcoded: it will actually adjust to the location of user

--- App.js
+++ App.js
@@ -1,28 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, View, ScrollView, Text, Button} from 'react-native';
+import {StyleSheet, View, Platform, ScrollView, Text, Button} from 'react-native';
 import React, { useState, useEffect } from 'react';
+import * as Location from 'expo-location';
 
 export default function App() {
 
   const [data, setData] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [counter, setCounter] = useState(false);
   
-  const lat = 34.4137076038152
-  const lng = -119.84548267425166
+  const [location, setLocation] = useState(null);
+  const [lat, setLat] = useState(34.4137076038152)
+  const [lng, setLng] = useState(-119.84548267425166)
+  const [alt, setAlt] = useState(-20)
 
   const nLat = lat + 0.5
   const nLng = lng + 0.5
 
   const sLat = lat - 0.5
   const sLng = lng - 0.5
+  
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setAlt(location.coords.altitude);
+      setLat(location.coords.latitude);
+      setLng(location.coords.longitude);
+      setLocation(location);
+    })();
+  }, [counter]);
+
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = "Your location:\nLatitude:" + lat + "\nLongitude:" + lng + "\nAltitude:" + alt + "\n";
+  }
 
   const datamap = data.map((item, index) => {
     return (
-      <Text key={index}>Hex:{item.hex} Lat:{item.lat} Lng:{item.lng}</Text>
+      <Text key={index}>Index:{index} Lat:{item.lat} Lng:{item.lng}</Text>
     )
   })
 
-
-  const api_key = "" // ASK FOR API KEY
+  const api_key = "" // Ask for API key
 
   const fetchApiCall = () => {
     fetch("https://airlabs.co/api/v9/flights?api_key="+api_key+"&bbox="+nLat+","+nLng+","+sLat+","+sLng+"&_fields=hex,lat,lng", {
@@ -38,7 +67,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Button title="Get Data" onPress={() => fetchApiCall()}/>
+      <Button title="Get Data" onPress={() => {setCounter(!counter);fetchApiCall()}}/>
+      <Text>{text}</Text>
       <ScrollView>{datamap}</ScrollView>
     </View>
   );
