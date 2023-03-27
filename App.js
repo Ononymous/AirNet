f514a1e125afd78b3d927ade7551c3a7b6d4d210
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2023-03-27 16:10:44 -0700

    Scrolling
    implement scrolling

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, Button, StyleSheet, Text, View } from 'react-native';
 import PlaneItem from './components/planeItem';
 
 //make to do list app that shows planes that are flying nearby
@@ -14,32 +14,32 @@ export default function App() {
       </View>
       <View style={styles.planesWrapper}>
         {/* <Text style={styles.sectionTitle}>Nearby Planes</Text> */}
-        
+        <ScrollView style={styles.ScrollView}>
 
-        <View style={styles.items}>
-          {/* This is where the planes will go */}
+          <View style={styles.items}>
+            {/* This is where the planes will go */}
 
-          <PlaneItem 
-          imageUrl={"https://media.wired.com/photos/62b25f4c18e6fafaa97a6477/16:9/w_2400,h_1350,c_limit/Air-Serbia-Plane-Russian-Sanctions-Safety-Hazard-Business-1239498184.jpg"}
-          airline="SouthWest"
-          flightNumber={"SWA2434"}
-          origin="LAX"
-          destination={"SBA"}
-          planeType={"Boeing 737"}
-          /> 
-        
-        <PlaneItem
-          imageUrl="https://e3.365dm.com/21/07/1600x900/skynews-boeing-737-plane_5435020.jpg?20210702173340"
-          airline="United Airlines"
-          flightNumber="UA123"
-          origin="SFO"
-          destination="JFK"
-          planeType="Boeing 737"
-        />
+            <PlaneItem 
+            imageUrl={"https://media.wired.com/photos/62b25f4c18e6fafaa97a6477/16:9/w_2400,h_1350,c_limit/Air-Serbia-Plane-Russian-Sanctions-Safety-Hazard-Business-1239498184.jpg"}
+            airline="SouthWest"
+            flightNumber={"SWA2434"}
+            origin="LAX"
+            destination={"SBA"}
+            planeType={"Boeing 737"}
+            /> 
+          
+          <PlaneItem
+            imageUrl="https://e3.365dm.com/21/07/1600x900/skynews-boeing-737-plane_5435020.jpg?20210702173340"
+            airline="United Airlines"
+            flightNumber="UA123"
+            origin="SFO"
+            destination="JFK"
+            planeType="Boeing 737"
+          />
 
-        
-        </View>
-
+          
+          </View>
+        </ScrollView>
       </View>
 
     </View>
@@ -47,6 +47,9 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  ScrollView: {
+    backgroundColor: '#C3C9E9',
+  },
   container: {
     flex: 1,
     backgroundColor: '#373F47',
