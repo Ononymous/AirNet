5585c2aa3059f7b6a10fdd142f31e8b7957369fe
Congrui Du <931638603@qq.com>
2023-03-31 23:28:01 -0700

    setting page

--- AirNet/Constant/infoStyles.js
+++ AirNet/Constant/infoStyles.js
@@ -69,6 +69,15 @@ const styles = StyleSheet.create({
         backgroundColor: '#373F47',
         
     },
+    userName:{
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 15,
+        marginTop: 0,
+        color:'#ffffff',
+        backgroundColor: '#373F47',
+        
+    },
     setFontSize: {
         fontSize: 20,
         fontWeight : 'bold' 
@@ -88,8 +97,97 @@ const styles = StyleSheet.create({
     backBtn:{
         flexDirection: 'row',
         justifyContent: 'center',
-    }
-      
+    },
+    profileImage:{
+       width: 100, 
+       height: 100, 
+       borderRadius: 400/ 2,
+       alignSelf:'center',
+       marginTop:10
+
+    },
+    searchInput: {
+        width: "100%",
+        height: "100%",
+        paddingHorizontal: SIZES.medium,
+        
+    },
+    searchWrapper: {
+        flex: 1,
+        backgroundColor: COLORS.gray2,
+        marginRight: SIZES.small,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: SIZES.medium,
+        height: "100%",
+        marginLeft:10
+      },
+      searchContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+        marginTop: SIZES.large,
+        height: 25,
+      },
+      searchBtn: {
+        width: 50,
+        height: "100%",
+        backgroundColor: COLORS.tertiary,
+        borderRadius: SIZES.medium,
+        justifyContent: "center",
+        alignItems: "center",
+        marginRight:10
+      },
+      searchBtnImage: {
+        width: "50%",
+        height: "50%",
+        tintColor: COLORS.white,
+      },
+      settingContainer:{
+        flexDirection: 'column',
+        alignContent:'center',
+        padding: 0,
+        borderRadius: 10,
+        marginBottom: 5,
+        marginHorizontal:2,
+        borderWidth: 5,
+        borderColor: '#373F47',
+      },
+      settingTab:{
+        flexDirection: 'row',
+        padding: 0,
+        borderRadius: 10,
+        marginBottom: 5,
+        marginHorizontal:2,
+        borderWidth: 0,
+        borderColor: '#373F47',
+        justifyContent:'space-between',
+        backgroundColor:'#D9D9D9',
+        marginTop:10,
+      },
+      settingTabText:{
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 17,
+        marginTop: 0,
+        color:COLORS.primary,
+        marginLeft:0,
+        marginTop:3,
+      },
+      switchStyle:{
+        marginRight:10,
+        marginTop:15,
+      },
+      settingIcons:{
+        marginRight:10,
+        marginTop:7,
+      },
+      signOutBtn:{
+        flexDirection: 'row',
+        justifyContent: 'center',
+      }
+
+    
       
 
 })
