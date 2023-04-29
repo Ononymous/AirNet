b400563bca9c82bcd1fe20547e6d8b4562e6391d
RayDu <97069739+CongruiDu@users.noreply.github.com>
2023-04-28 20:45:19 -0700

    Changed BG color of Moreinfo Page

--- tabs/MoreInfo.js
+++ tabs/MoreInfo.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Image, ScrollView, Dimensions } from 'react-nat
 import FlightInfo from '../components/FlightInfo';
 import { useEffect } from 'react';
 import SessionContext from '../backend/SessionContext';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import HeartButton from '../components/HeartButton';
 import CameraButton from '../components/CameraButton';
@@ -24,29 +25,37 @@ export default function MoreInfo({route, navigation}) {
       // ,
     });
   }, [navigation, plane.flightNumber]);
-  
+
   return (
+  
     <View style={styles.newContainer}>
+      <LinearGradient colors={['#a6fde8', '#acb6e5']} style={styles.container}>
       <ScrollView>
         <View style={styles.container}>
           <Image source={{ uri: plane.imgUrl }} style={styles.image} />
+ 
           <View style={styles.infoContainer}>
             <FlightInfo str='Aircraft Type'/>
             <Text style={styles.BiggerText}>{plane.planeType + '\n'}</Text>
           </View>
+
+
           <View style={styles.infoContainer}>
             <FlightInfo str='Route'/>
             <Text style={styles.originText}>{plane.originFull + ' (' + plane.origin + ')\n'} to {'\n' + plane.destinationFull + ' (' + plane.destination + ')\n'}</Text>
           </View>
+
           {/* <View style={styles.infoContainer}>
             <FlightInfo str='Scheduled Arrival Time '/>
             <Text style={styles.BiggerText}>{plane.scheduledArrival}{'\n'}</Text>
           </View> */}
+
           <View style={styles.infoContainer3}>
             <View style={styles.infoContainer} width={110}>
               <FlightInfo str='Altitude'/>
               <Text style={styles.NUM}>{plane.altitude} ft {'\n'}</Text>
             </View>
+            
             <View style={styles.infoContainer} width={110}>
               <FlightInfo str='Latitude'/>
               <Text style={styles.NUM}>{plane.latitude} {'\n'}</Text>
@@ -56,21 +65,25 @@ export default function MoreInfo({route, navigation}) {
               <Text style={styles.NUM}>{plane.longitude} {'\n'}</Text>
             </View>
           </View>
+
+
           <View style={styles.infoContainer}>
             <FlightInfo str='Aircraft Speed'/>
             <Text style={styles.BiggerText}>{plane.speed} Knots {'\n'}</Text>
           </View>
+
         </View>     
+        
       </ScrollView>
-      <View style={styles.backBtn}>
-      </View>
+      </LinearGradient>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   newContainer:{
-    backgroundColor: '#373F47',
+
     height: '100%',
   },
 
@@ -79,23 +92,21 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     alignContent:'center',
     padding: 5,
-    backgroundColor: '#D9D9D9',
-    borderRadius: 10,
-    marginBottom: 10,
-    borderWidth: 5,
-    borderColor: '#373F47',
-    gap:10,
+    borderRadius: 0,
+    marginBottom: 0,
+    borderWidth: 0,
+    gap:'28%',
   },
   infoContainer:{
     flexDirection: 'column',
     alignContent:'center',
-    padding: 0,
-    backgroundColor: '#2C4251',
+    padding: 3,
+    backgroundColor:'#E7FDF6',
     borderRadius: 10,
     marginBottom: 5,
     marginHorizontal:4,
-    borderWidth: 5,
-    borderColor: '#373F47',
+    borderWidth: 0,
+
   },
   image: {
     width: Dimensions.get('window').width - 28,
@@ -107,10 +118,9 @@ const styles = StyleSheet.create({
   },
   infoContainer3:{
     flexDirection: 'row',
-    padding: 0,
-    backgroundColor: '#D9D9D9',
+    padding: 3,
     marginBottom: 0,
-    borderColor: '#373F47',
+
     justifyContent:"space-evenly"
   },
   HEADERVIEW:{
@@ -123,24 +133,22 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 25,
     marginTop: 0,
-    color:'#ffffff',
-    backgroundColor: '#373F47',
+    color:'#36454F',
   },
   originText: {
     textAlign: 'center',
     fontWeight: 'bold',
     fontSize: 15,
     marginTop: 0,
-    color:'#ffffff',
-    backgroundColor: '#373F47',
+    color:'#36454F',
+
   },
   NUM:{
     textAlign: 'center',
     fontWeight: 'bold',
     fontSize: 17,
     marginTop: 0,
-    color:'#ffffff',
-    backgroundColor: '#373F47',
+    color:'#36454F',
 
   },
   backBtn:{
